87b3c4685f3e3582cd2604e57d30c3df3c0ab0b8
KaloyanSvilenov <kaloyansv@gmail.com>
2024-07-19 15:02:23 +0300

    again adjusted docs folders

--- javaDocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ConsoleCommands"},{"l":"ConsoleCommands.Enum"},{"l":"ConsoleCommands.FileManagment"},{"l":"ConsoleCommands.Interface"},{"l":"ConsoleCommands.XMLManagment"},{"l":"Parser"}];updateSearchResults();
